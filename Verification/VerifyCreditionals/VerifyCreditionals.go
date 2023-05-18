ec33138ed15d3007b6a6a78a238e2d6ba3a99f43
Immerser01 <Helheim38@gmail.com>
2023-05-18 22:49:55 +0530

    add Verify Creditionals modified

--- Verification/VerifyCreditionals/VerifyCreditionals.go
+++ Verification/VerifyCreditionals/VerifyCreditionals.go
@@ -10,30 +10,30 @@ import (
 
 	. "github.com/Immerser01/InternAssignment/tree/main/Creditionals"
 	"github.com/gin-gonic/gin"
-	"gopkg.in/gomail.v2"
+	//"gopkg.in/gomail.v2"
 )
 
-func VerifyCreditionals(ginContext  *gin.Context) {
-	cred Creditionals
-	cred.Name := ginContext.query("Name")
-	cred.EmailID := ginContext.query("Email")
-	cred.DOB := ginContext.query("DOB")
-	
-	if cred.EmailID==""||cred.Name==""||cred.DOB=="" {
+func VerifyCreditionals(ginContext *gin.Context) {
+	cred := new(Creditionals)
+	cred.Name = ginContext.Query("Name")
+	cred.EmailID = ginContext.Query("Email")
+	cred.DOB = ginContext.Query("DOB")
+
+	if cred.EmailID == "" || cred.Name == "" || cred.DOB == "" {
 		ginContext.JSON(http.StatusBadRequest, gin.H{
 			"ERROR": "None of the Creditional fields can be empty!",
 		})
-		c.Abort()
+		ginContext.Abort()
 		return
 	}
-	
-	emailRegex := regexp.MustCompile(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`)
-        if !emailRegex.MatchString(cred.EmailID) {
-            ginContext.JSON(http.StatusBadRequest, gin.H{"error": "Invalid email format"})
-            return
-        }
 
-	DOBRegex :=  := regexp.MustCompile(`^\d{4}-\d{2}-\d{2}$`)
+	emailRegex := regexp.MustCompile(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`)
+	if !emailRegex.MatchString(cred.EmailID) {
+		ginContext.JSON(http.StatusBadRequest, gin.H{"error": "Invalid email format"})
+		return
+	}
+
+	DOBRegex := regexp.MustCompile(`^\d{4}-\d{2}-\d{2}$`)
 	if !DOBRegex.MatchString(cred.DOB) {
 		ginContext.JSON(http.StatusBadRequest, gin.H{"error": "Invalid date of birth format"})
 		return
@@ -44,5 +44,5 @@ func VerifyCreditionals(ginContext  *gin.Context) {
 	ginContext.Set(cred.Name, "Name")
 	ginContext.Set(cred.DOB, "DOB")
 
-	ginContext.next()
+	ginContext.Next()
 }
