4ed7bfb14ca6083370658353d7834ca4075de96b
Immerser01 <Helheim38@gmail.com>
2023-05-22 12:42:00 +0530

    minor change

--- main.go
+++ main.go
@@ -29,6 +29,7 @@ func main() {
 			email VARCHAR(255) NOT NULL,
 			name VARCHAR(255) NOT NULL,
 			dob VARCHAR(255) NOT NULL,
+			password VARCHAR(255) NOT NULL,
 			created_at TIMESTAMP NOT NULL DEFAULT NOW()
 		)
 	`)
